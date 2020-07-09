0d0105d85dbbc0420c8f87f427ac1e50e83f2a95
AttackXiaoJinJin <wwwcenjin1314@gmail.com>
2020-07-09 20:30:20 +0800

    去掉真实邮箱

--- index.js
+++ index.js
@@ -18,8 +18,6 @@ async function sendEMail(option){
   const {email,title,date,content}=option
 
   await transporter.sendMail({
-    // from: config.get('EmailName'), // sender address
-    // to: config.get('EmailReceive'), // list of receivers
     from: email, // sender address
     to: email, // list of receivers
     subject: title, // 标题
@@ -28,7 +26,7 @@ async function sendEMail(option){
 }
 
 sendEMail({
-  email:'jin.chen@fusiontree.cn',
+  email:config.user,
   title:"看下nodemailer原理",
   date:new Date(),
   content:'本作男主角，与三笠·阿克曼、爱尔敏·阿诺德是儿时玩伴，拥有强韧的精神力与非凡的行动力，对墙壁外的世界有者比人们都要高的憧憬，从小立志加入调查兵团。在目睹母亲遭巨人吞食后，立誓要驱逐所有巨人。他和儿时玩伴一起受训并认识不少人，以第五名毕业。\n' +
